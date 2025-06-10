c40dd78bb79eff2c5e1707ef3a554b786292ddde
DangGiaThuan <danggiathuanhl@gmail.com>
2025-06-10 14:30:14 +0700

    Update

--- actions/actions.py
+++ actions/actions.py
@@ -624,3 +624,13 @@ class ActionClearSlots(Action):
             SlotSet("location", None),
             SlotSet("salary", None)
         ]
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(response="utter_default")
+        return []
